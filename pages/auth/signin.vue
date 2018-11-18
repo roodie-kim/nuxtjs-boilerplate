fd95c59b289c122c9b942e8fc42c4040b73237b1
hkahn <rood_optical@naver.com>
2018-11-18 20:59:49 +0900

    Initial commit

---
+++ pages/auth/signin.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <form @submit.prevent="signUp">
+            <input
+                v-model="user.email"
+                type="email"
+                name="email"
+                placeholder="email">
+            <input
+                v-model="user.password"
+                type="password"
+                name="password"
+                placeholder="password">
+            <div>
+                <button type="submit">Sign In</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            user: {
+                email: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        async signIn () {
+            const response = await this.$store.dispatch('signIn', this.user)
+            if (response.status) {
+                this.$store.dispatch('readUser')
+            }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/auth/signup.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <form @submit.prevent="signUp">
+            <input
+                v-model="user.email"
+                type="email"
+                name="email"
+                placeholder="email">
+            <input
+                v-model="user.password"
+                type="password"
+                name="password"
+                placeholder="password">
+            <input
+                v-model="user.name"
+                type="text"
+                name="name"
+                placeholder="name">
+            <div>
+                <button type="submit">Sign Up</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            user: {
+                email: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        async signUp () {
+            const response = await this.$store.dispatch('signUp', this.user)
+            if (response.status) {
+                this.$store.dispatch('readUser')
+            }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
